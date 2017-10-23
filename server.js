f4e5adae4970c0264df04023bc264d48d5f0ea3a
Matt Larson <matt.a.larson@gmail.com>
2017-10-23 17:41:09 -0500

    tweaked server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(PORT, function(req, res) {
