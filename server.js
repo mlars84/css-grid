ae68685b03e1b8436017740c8d5c4159cd30c637
Matt Larson <matt.a.larson@gmail.com>
2017-10-23 17:34:15 -0500

    added quick server for deployment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const app = express()
+const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + 'public/index.html'));
+});
+
+app.listen(PORT, function(req, res) {
+  console.log(`listening on ${PORT}`)
+})
